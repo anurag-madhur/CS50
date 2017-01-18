5987bbf73af45ac719aecc5f5d0b2e9492f0416f
Anurag Madhur <anurag.madhur@chd.trantorinc.com>
2017-01-18 17:30:40 +0530

    problem set1

--- week1/greedy.c
+++ week1/greedy.c
@@ -1,46 +1,46 @@
 #include<stdio.h>
+#include<stdlib.h>
 int main()
 {
       int n,count=0;
-      int x;
-      printf("enter the amount to be returned :");
-      scanf("%d",&x);
-if(x>0)
-{
-      while(x!=0)
-{	
-      if(x>=25)
-        {
-          n=x/25;
-          x=x%25;
-          count=count+n;
-        }
+      char str[50];
+      float x;
+      do
+      {
+        printf("o hai! How much change is owed?\n");
+      	fgets(str,50,stdin);
+      	x=atof(str);
+      	if(x==0)
+        printf("retry\n");
+      }while(x<=0);
 
-      if(x>=10)
-        {
-          n=x/10;
-          x=x%10;
-          count=count+n;
-        }
+	  while(x>0.000001)
+	  {
+		if(x>=.25)
+		{
+		    x=x-.25;
+			count++;
+		}
+		if(x>=.10&&x<.25)
+		{
+			x=x-.10;
+			count++;
+		}
+		if(x>=.05&&x<.10)
+		{
+			x=x-.05;
+			count++;
+		}
+		if(x>=.009999&&x<.05)
+		{
+			x=x-.01;
+			count++;
+		}
+		printf("%f",x);
+	  }
+   	printf("minimum number of coins are :%d\n",count);
 
-       if(x>=5)
-       {
-          n=x/5;
-          x=x%5;
+    else
+    printf("plzz enter valid amount.");
 
-          count=count+n;
-
-       }
-
-       if(x<5)
-       {
-        count=count+x;
-        x=0;
- }    
-}
-       printf("minimum number of coins are :%d\n",count);
-
-}
-else
-printf("plzz enter valid amount.");
 }
